4fc6fa02ff89f92cc5fc0a2c5e2e07f86ddea8ef
yoheitaonishi <onishiyoheita@gmail.com>
2019-05-15 21:35:40 +0900

    update file

--- 4_led/led_chikachika.py
+++ 4_led/led_chikachika.py
@@ -4,10 +4,15 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.OUT)
 
-while True:
-    GPIO.output(25, GPIO.HIGH)
-    sleep(0.5)
-    print('HIGH')
-    GPIO.output(25, GPIO.LOW)
-    sleep(0.5)
-    print('LOW')
+try:
+    while True:
+        GPIO.output(25, GPIO.HIGH)
+        sleep(0.5)
+        print('HIGH')
+        GPIO.output(25, GPIO.LOW)
+        sleep(0.5)
+        print('LOW')
+except KeyboardInterrupt:
+    pass
+
+GPIO.cleanup()
