c4a629ece6281991974a89efefb9c0f6fa444ed4
yoheitaonishi <onishiyoheita@gmail.com>
2019-05-16 12:20:09 +0900

    added 5

---
+++ 5_taktswitch/pulldown.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+try:
+    while True:
+        if GPIO.input(24) == GPIO.HIGH:
+            GPIO.output(25, GPIO.HIGH)
+        else:
+            GPIO.output(25, GPIO.LOW)
+        sleep(0.01) # you need a lot of memory if you don't use sleep.
+
+except KeyboardInterrupt:
+    pass
+
+GPIO.cleanup()
---
+++ 5_taktswitch/toggle.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+def my_callback(channel):
+    global ledState
+    if channel == 24:
+        ledState = not ledState
+        if ledState == GPIO.HIGH:
+            GPIO.output(25, GPIO.HIGH)
+        else:
+            GPIO.output(25, GPIO.LOW)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(24, GPIO.RISING, callback=my_callback, bouncetime=200)
+
+ledState = GPIO.LOW
+
+try:
+    while True:
+        sleep(0.01)
+
+except KeyboardInterrupt:
+    pass
+
+GPIO.cleanup()
+
